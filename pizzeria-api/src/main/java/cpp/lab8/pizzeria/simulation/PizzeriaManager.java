e64b0fb994220b0e1b05cedcb158a491c7286617
Roman Tkachenko <rt4freel@gmail.com>
2022-11-15 02:11:05 +0200

    customer refactoring to make more use of the facade

--- pizzeria-api/src/main/java/cpp/lab8/pizzeria/simulation/PizzeriaManager.java
+++ pizzeria-api/src/main/java/cpp/lab8/pizzeria/simulation/PizzeriaManager.java
@@ -15,6 +15,10 @@ import org.springframework.stereotype.Service;
 import cpp.lab8.pizzeria.simulation.configuration.PizzeriaConfiguration;
 import cpp.lab8.pizzeria.simulation.customer.Customer;
 import cpp.lab8.pizzeria.simulation.customer.CustomerSystem;
+import cpp.lab8.pizzeria.simulation.customer.generateStrategy.ClientGenerator;
+import cpp.lab8.pizzeria.simulation.customer.generateStrategy.EveningStrategy;
+import cpp.lab8.pizzeria.simulation.customer.generateStrategy.MorningStrategy;
+import cpp.lab8.pizzeria.simulation.customer.generateStrategy.RushHourStrategy;
 import cpp.lab8.pizzeria.simulation.order.OrderSystem;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,12 +35,16 @@ public class PizzeriaManager {
     // current configuration
     private PizzeriaConfiguration configuration;
 
+    // client generation strategy
+    private ClientGenerator clientGenerator;
+
     // all subsystems
     private final CustomerSystem customerSystem;
     private final OrderSystem orderSystem;
     private final PizzaSystem pizzaSystem;
     private final CookSystem cookSystem;
     private final QueueSystem queueSystem;
+    
     @Autowired
     public PizzeriaManager(CustomerSystem cs, OrderSystem os,  PizzaSystem ps, CookSystem cks, QueueSystem qs) {
         customerSystem = cs;
@@ -48,6 +56,14 @@ public class PizzeriaManager {
 
     // start workflow with current configuration
     public void start() {
+        // configure client generation strategy
+        switch (configuration.getStrategy()) {
+            case 1:this.clientGenerator = new RushHourStrategy(); break;
+            case 2: this.clientGenerator = new EveningStrategy(); break;
+            default: this.clientGenerator = new MorningStrategy(); break;
+        }
+
+        // start systems
         customerSystem.startGeneration(configuration.getVisitorsTimeout());
         queueSystem.createQueues(configuration.getCashRegisters());
 
@@ -61,10 +77,15 @@ public class PizzeriaManager {
         cookSystem.createCooks(configuration.getCooks(), configuration.getCookStrategy());
     }
 
+    // customer related
+    public synchronized void generateCustomer() {
+        this.clientGenerator.generateCustomer(this);
+    }
     public synchronized void chooseQueue(Customer customer) {
         queueSystem.assignCustomerToShortestQueue(customer);
     }
 
+    // cook related
     public synchronized Pizza findPizzaToCook(PizzaState pizzaState){
         Pizza pizza = null;
         
